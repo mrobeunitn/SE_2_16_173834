eb1df46e2f8439ff8bb3378fee1155e536f0f4f6
mrobeunitn <marco.roberti@studenti.unitn.it>
2016-11-14 18:14:58 +0100

    Tabella

---
+++ functionjs.js
@@ -0,0 +1,11 @@
+ 
+var items = [{"type":"Smartohine", "qta":"2"},{"type":"PC", "qta": "8" }];
+
+
+function visualizzaTabella(){
+    var stringa = "";
+    for(int i = 0 ; i< items.length ; i++ ){
+        
+        
+    }
+}
