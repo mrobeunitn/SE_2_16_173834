224ffcc6db243f920206faa9e65640b2ad35cdef
mrobeunitn <marco.roberti@studenti.unitn.it>
2016-11-15 13:28:29 +0100

    Funzione Ordina Modificata Dom

--- functionjs.js
+++ functionjs.js
@@ -6,26 +6,31 @@ var items = [{"type":"Smartphone", "qta": 2 },{"type":"PC", "qta": 8 }];
 //funzione che mi ritorna la tabella precompilata
 function visualizzaTabella(){
     //mi creo una variabile stringa di html tag condegli indici. per ogni item dell'array vado a sostituire il relativo valore
-    var stringa = "<tr><td>{item}</td><td>{qta}</td></tr>";
-    var htmlstring="<table><tr><td align='center'><b>Object</b></td><td align='center'><b>Quantity</b></td></tr> ";
+    var table = document.getElementById("tabella");
     for(var i = 0 ; i < items.length; i++ ){
-        htmlstring+= "<tr><td align='center'>"+items[i].type+"</td><td align='center'>"+items[i].qta+"</td></tr>"
-        //stringa.replace(item,items[i].type).replace(qta,items[i].qta);
+        var row = table.insertRow(-1); 
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell1.innerHTML = items[i].type;
+        cell2.innerHTML = items[i].qta;
     }
-    htmlstring+="</table> <h4 align='right'>Max Order Item : 30</h4>";
-    document.write(htmlstring+"<br>");
-    document.getElementById("tabella").innerHTML = htmlstring ;
-    htmlstring = "<input type='button' onclick='getForms()' value='Ordia item'>";
-    document.getElementById("OrderForm").innerHTML = "<input type='button' onclick='getForms()' value='Ordia item'>";
 }
 
 //fuznione per disegnare il form per ordinare 
-
 function getForms(){
-    htmlstring = "<input type='text' id='item' placeholder='item' required>";
-    htmlstring+= "<input type='text' id='qta' placeholder='qta' required>";
-    htmlstring+="<input type='button' name='order' value='send' onclick='ordina()'>";
-    document.getElementById("OrderForm").innerHTML = htmlstring ;
+    document.getElementById("ordina").style.display ='block';
+}
+//nascondo il form ordina
+function hideButtonOrdina(){
+     document.getElementById("ordina").style.display ='none';
+}
+//visualizzo bottone per generare il campo per modificare qta max
+function getForm(){
+    document.getElementById("ordina").style.display ='block';
+}
+//nascondo qta max generator
+function hideButtonOrdina(){
+     document.getElementById("ordina").style.display ='none';
 }
 
 //funzione per la gestione dell'ordinee
@@ -48,7 +53,8 @@ function ordina(){
         alert("Ordine effettuato");
     }
     else{
-      item.append({"type": item, "qta": qta});
+      var temp = {"type": item, "qta": qta};
+      item.push(temp);
       alert("Ordine Effettuato");
     }
     visualizzaTabella();
