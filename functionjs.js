21571384c13e30631e8cf8804839b22d8d47e54a
mrobeunitn <marco.roberti@studenti.unitn.it>
2016-11-15 12:39:35 +0100

    Funzione Ordina

--- functionjs.js
+++ functionjs.js
@@ -1,6 +1,6 @@
 
 var maxvalue = 30;
-var items = [{"type":"Smartphone", "qta":"2"},{"type":"PC", "qta": "8" }];
+var items = [{"type":"Smartphone", "qta": 2 },{"type":"PC", "qta": 8 }];
 
 
 //funzione che mi ritorna la tabella precompilata
@@ -15,15 +15,42 @@ function visualizzaTabella(){
     htmlstring+="</table> <h4 align='right'>Max Order Item : 30</h4>";
     document.write(htmlstring+"<br>");
     document.getElementById("tabella").innerHTML = htmlstring ;
-    return htmlstring;
+    htmlstring = "<input type='button' onclick='getForms()' value='Ordia item'>";
+    document.getElementById("OrderForm").innerHTML = "<input type='button' onclick='getForms()' value='Ordia item'>";
 }
 
 //fuznione per disegnare il form per ordinare 
 
 function getForms(){
-    htmlstring = "<input type='text' name='item' placeholder='item'>";
-    htmlstring+= "<input type='text' name='qta' placeholder='qta'>";
+    htmlstring = "<input type='text' id='item' placeholder='item' required>";
+    htmlstring+= "<input type='text' id='qta' placeholder='qta' required>";
     htmlstring+="<input type='button' name='order' value='send' onclick='ordina()'>";
     document.getElementById("OrderForm").innerHTML = htmlstring ;
+}
+
+//funzione per la gestione dell'ordinee
+
+function ordina(){
+    var item = document.getElementById("item").value;
+    var qta = document.getElementById("qta").value;
+    var i =0;
+    var trovato = false;
+    var pos = 0;
+    while( i < items.length && trovato == false ){
+        if(items[i].type === item){
+            trovato = true;
+            pos = i;
+        }
+        i++;
+    }
+    if(trovato == true){
+        items[pos].qta += qta;
+        alert("Ordine effettuato");
+    }
+    else{
+      item.append({"type": item, "qta": qta});
+      alert("Ordine Effettuato");
+    }
+    visualizzaTabella();
 }
 
